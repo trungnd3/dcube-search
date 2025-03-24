fbb268d3bcf3fa3434101130f5d9c0b395caf714
Nguyen Duc Trung <ductrung.ftu@gmail.com>
2025-03-24 08:54:30 +0700

    add unit tests

--- src/lib/util.test.tsx
+++ src/lib/util.test.tsx
@@ -1,14 +1,25 @@
-import { documents } from "../../tests/mock"
-import { cn, findSuggestions } from "./util"
+import { documents } from '../../tests/mock';
+import { cn, filterData, findSuggestions } from './util';
 
 describe('util', () => {
   it('merges class names with cn function', () => {
-    expect(cn('class-1', 'class-2')).toBe('class-1 class-2')
-  })
+    expect(cn('class-1', 'class-2')).toBe('class-1 class-2');
+  });
 
   it('returns suggestion list', () => {
-    const searchTerm = 'testsuggestion'
+    const searchTerm = 'testsuggestion';
     documents.ResultItems[0].DocumentTitle.Text = `${documents.ResultItems[0].DocumentTitle} testing ${searchTerm} testing testing ${searchTerm} testing`;
-    expect(findSuggestions(searchTerm, documents)).toContain(`testing ${searchTerm} testing`)
-  })
-})
+    expect(findSuggestions(searchTerm, documents)).toContain(
+      `testing ${searchTerm} testing`
+    );
+  });
+
+  it('filter data based on search term', () => {
+    const searchTerm = 'testsuggestion';
+    const resultTitle = `${documents.ResultItems[0].DocumentTitle} testing ${searchTerm} testing testing ${searchTerm} testing`;
+    documents.ResultItems[0].DocumentTitle.Text = resultTitle;
+    expect(filterData(searchTerm, documents).ResultItems[0].DocumentTitle.Text).toEqual(
+      resultTitle
+    );
+  });
+});
